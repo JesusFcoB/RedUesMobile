59164e1338ced39c691d5ee3c2f9bfef1c7fe1bf
Brayan Quintero <josebrayanquinterocampos@gmail.com>
2026-04-26 15:17:15 -0700

    feat: agrega DTOs para las requests/responses de la api

---
+++ app/src/main/java/com/example/reduesmobile/data/dto/LikesResponse.kt
@@ -0,0 +1,6 @@
+package com.example.reduesmobile.data.dto
+
+data class LikesResponse(
+    val likes: Int,
+    val dioLike: Boolean,
+)
---
+++ app/src/main/java/com/example/reduesmobile/data/dto/PublicacionResponse.kt
@@ -0,0 +1,22 @@
+package com.example.reduesmobile.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class PublicacionResponse(
+    @SerializedName("publicacion_id")
+    val idPublicacion: Int,
+
+    @SerializedName("autor_id")
+    val idAutor: Int,
+    val autor: String,
+    val contenido: String,
+    val tipo: String,
+
+    @SerializedName("fecha_creacion")
+    val fechaCreacion: String,
+    val comentarios: List<ComentarioResponse> = emptyList(),
+    val cantidadLikes: Int,
+    val yaDioLike: Boolean,
+    val cantidadGuardados: Int,
+    val yaGuardo: Boolean
+)
