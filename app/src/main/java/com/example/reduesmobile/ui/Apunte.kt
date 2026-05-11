480e1b996e7cc9b94679eb383e3d27e3287ad744
Brayan Quintero <josebrayanquinterocampos@gmail.com>
2026-05-11 00:42:44 -0700

    feat: agrega CkEditor5 para la creacion de apuntes

--- app/src/main/java/com/example/reduesmobile/ui/Apunte.kt
+++ app/src/main/java/com/example/reduesmobile/ui/Apunte.kt
@@ -1,6 +1,8 @@
 package com.example.reduesmobile.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,28 @@ class Apunte : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityApunteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupEditor()
+
+        NavigationHelper.setupBottomNavigation(this, binding.bottomNavigation)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupEditor() {
+        binding.webViewEditor.apply {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true // Necesario para CKEditor
+
+            // Cargamos el archivo local de assets
+            loadUrl("file:///android_asset/editor.html")
+        }
+    }
+
+    private fun obtenerTextoDelEditor() {
+        // Ejecutamos la función JavaScript definida en el HTML
+        binding.webViewEditor.evaluateJavascript("editorInstance.getData();") { htmlContent ->
+            // El htmlContent viene entre comillas (ej: "<p>Hola</p>")
+            val cleanHtml = htmlContent.removeSurrounding("\"").replace("\\u003C", "<")
+            Log.d("EDITOR", "Contenido HTML: $cleanHtml")
+        }
     }
 }
